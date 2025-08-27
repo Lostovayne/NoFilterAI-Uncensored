52af4aeb1287325a1fba53c50b7be7521ad3a8e6
LostoVayne <moises.barrera.dev@gmail.com>
2025-08-26 23:01:12 -0400

    refactor: standardize code formatting and improve readability across the client and server packages
    - Updated README.md for clearer ESLint configuration instructions.
    - Reformatted JSON files for consistent indentation and style.
    - Enhanced ESLint configuration for better linting rules and options.
    - Adjusted HTML structure in index.html for consistent formatting.
    - Refined package.json files for better readability and organization.
    - Improved CSS styles in App.css for consistency.
    - Standardized import statements and formatting in TypeScript files.
    - Updated TypeScript configuration files for better clarity and structure.
    - Refactored server index.ts for improved code consistency and readability.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <>
-      <p className="font-bold">{message}</p>
-      <Button className="mt-4" variant="outline">
-        Click Me
-      </Button>
-    </>
-  );
+   return (
+      <>
+         <p className="font-bold">{message}</p>
+         <Button className="mt-4" variant="outline">
+            Click Me
+         </Button>
+      </>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import type { Request, Response } from "express";
-import express from "express";
-import dotenv from "dotenv";
+import type { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({ status: "Server is running" });
+app.get('/', (req: Request, res: Response) => {
+   res.send({ status: 'Server is running' });
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.send({ message: "Hello World" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send({ message: 'Hello World' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
